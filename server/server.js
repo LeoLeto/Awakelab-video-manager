4a3ccdf2378e6d7272cb6e095ca9bc558ad7bc0b
LeoLeto <132474553+LeoLeto@users.noreply.github.com>
2025-12-11 07:54:55 -0300

    Increase request body size limits for JSON and URL-encoded data

--- server/server.js
+++ server/server.js
@@ -25,7 +25,8 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
-app.use(express.json());
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
 
 // Initialize S3 client
 const s3Client = new S3Client({
